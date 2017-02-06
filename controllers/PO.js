afeb40eb064a218db8769481750bf363ada2e465
Gonzalo Jiménez Balsa <gjimenez@gradiant.org>
2017-02-06 20:29:38 +0100

    - Added method '/processstatus/{processInstanceId}' to PO dummy service.

--- controllers/PO.js
+++ controllers/PO.js
@@ -9,3 +9,7 @@ var PO = require('./POService');
 module.exports.executeProtectionConfiguration = function executeProtectionConfiguration (req, res, next) {
   PO.executeProtectionConfiguration(req.swagger.params, res, next);
 };
+
+module.exports.processStatus = function processStatus (req, res, next) {
+  PO.processStatus(req.swagger.params, res, next);
+}
--- controllers/POService.js
+++ controllers/POService.js
@@ -2,6 +2,15 @@
 var sleep = require('sleep');
 var request = require('superagent');
 
+var responses = [{code: 0, message: 'STATE_PENDING'},
+    {code: 1, message: 'STATE_ACTIVE'},
+    {code: 2, message: 'STATE_COMPLETED'},
+    {code: 3, message: 'STATE_ABORTED'},
+    {code: 4, message: 'STATE_SUSPENDED'}
+];
+
+var processes = [];
+
 exports.executeProtectionConfiguration = function(args, res, next) {
   /**
    * parameters expected in the args:
@@ -10,42 +19,76 @@ exports.executeProtectionConfiguration = function(args, res, next) {
   * purpose (String)
   * serviceCallParameters (RequestPO)
   **/
+  var processInstanceId = processes.length;
+
+  console.log("processInstanceId: " + processInstanceId);
 
   res.setHeader('Content-Type', 'text/plain');
-  res.end("1");
+  res.end('' + processInstanceId);
+
+  processes.push(responses[0]);
 
   console.log('executeProtectionConfiguration');
-  sleep.sleep(__config.po_sleep);
+  setTimeout(function() {
 
-  var url = __config.brokerUrlPrefix + args.serviceCallParameters.value.callbackUrl;
-  console.log(url);
 
-  var agent = request.agent();
+    processes[processInstanceId] = responses[1];
 
-  agent.post(url)
-  .set('Content-Type', 'application/json')
-  .send(
-    { 
-        status: 'success',
-        results: [
-            {
-                key: "modifiedServiceParams",
-                value: args.serviceCallParameters.value.serviceCallParameters 
+    setTimeout(function() {
+        processes[processInstanceId] = responses[2];
+
+        var url = __config.brokerUrlPrefix + args.serviceCallParameters.value.callbackUrl;
+        console.log(url);
+
+        var agent = request.agent();
+
+        agent.post(url)
+        .set('Content-Type', 'application/json')
+        .send(
+            { 
+                status: 'success',
+                results: [
+                    {
+                        key: "modifiedServiceParams",
+                        value: args.serviceCallParameters.value.serviceCallParameters 
+                    }
+                ]
             }
-        ]
-    }
-  )
-  .end(function(error, response) {
-    if (error) {
-        console.log("error:" + error);
-        if (response) {
-          console.log(response.status);
-          console.log(response.body);
-        }
-    } else {
-        console.log(response.body);
-    }
-  });
-  
+        )
+        .end(function(error, response) {
+            if (error) {
+                console.log("error:" + error);
+                if (response) {
+                console.log(response.status);
+                console.log(response.body);
+                }
+            } else {
+                console.log(response.body);
+            }
+        });
+    }, __config.po_sleep * 1000);
+
+
+  }, __config.po_sleep * 1000);  
+}
+
+exports.processStatus = function(args, res, next) {
+ /**
+  * parameters expected in the args:
+  * xAuthToken (String)
+  * processInstanceId (int64)
+  **/
+
+  //console.log("processInstanceId: " + args.processInstanceId.value);
+
+  var po_status = processes[args.processInstanceId.value];
+
+  if (po_status) {
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(po_status, null, 2));
+  } else {
+      res.writeHead(404);
+      res.end();
+  }
 }
 

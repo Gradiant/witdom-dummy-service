14e8c55034538d3cf350c29d73d4f85da343cf1f
Gonzalo Jiménez Balsa <gjimenez@gradiant.org>
2017-02-07 18:54:22 +0100

    - Minor changes in broker host config.

--- config/default.js
+++ config/default.js
@@ -1,9 +1,9 @@
 module.exports = {
     http: { port: 8080 },
     brokerProtocol: "http",
-    brokerHost: "broker",
+    brokerHost: "broker-td",
     brokerPort: "5000",
-    brokerUrlPrefix: "http://broker:5000/v1",
+    brokerUrlPrefix: "http://broker-td:5000/v1",
     po_sleep: 2,
     service_td_sleep: 2,
     service_ud_sleep: 2
